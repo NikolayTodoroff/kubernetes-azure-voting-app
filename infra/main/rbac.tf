7c1efe936c204927317ae3436b4fa464ffa185d1
Nikolay Todorov <everflowing555@gmail.com>
2026-06-25 19:28:52 +0300

    v1

--- infra/main/rbac.tf
+++ infra/main/rbac.tf
@@ -3,3 +3,15 @@ resource "azurerm_role_assignment" "acr_kubernetes_pull" {
   role_definition_name = "AcrPull"
   principal_id         = module.aks.kubelet_identity_object_id
 }
+
+resource "azurerm_role_assignment" "acr_pipeline_push" {
+  scope                = module.container_registry.acr_id
+  role_definition_name = "AcrPush"
+  principal_id         = var.pipeline_sp_object_id
+}
+
+resource "azurerm_role_assignment" "aks_pipeline_admin" {
+  scope                = module.aks.cluster_id
+  role_definition_name = "Azure Kubernetes Service Cluster Admin Role"
+  principal_id         = var.pipeline_sp_object_id
+}
--- infra/main/variables.tf
+++ infra/main/variables.tf
@@ -23,3 +23,8 @@ variable "aks_admin_group_object_id" {
   description = "Object ID of the Azure AD group granted cluster-admin access"
   type        = string
 }
+
+variable "pipeline_sp_object_id" {
+  description = "Object ID of the service principal used by the CI/CD pipeline"
+  type        = string
+}
